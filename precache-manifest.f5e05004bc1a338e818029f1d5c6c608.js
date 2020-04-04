68048d64a83e9230480429b09500f2b591e59c29
Alexander <elementoffchaos@gmail.com>
2020-04-04 12:55:24 +0700

    Updates

---
+++ precache-manifest.f5e05004bc1a338e818029f1d5c6c608.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928345f04e6776fa74308eaf5e30d236",
+    "url": "/retro-sunset/index.html"
+  },
+  {
+    "revision": "6d0d2c29c4870df718f6",
+    "url": "/retro-sunset/static/js/2.7a560f60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/retro-sunset/static/js/2.7a560f60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38fae077d0cbecbd33d0",
+    "url": "/retro-sunset/static/js/main.67b4265b.chunk.js"
+  },
+  {
+    "revision": "769e30dc019c7806813e",
+    "url": "/retro-sunset/static/js/runtime-main.e392a8e5.js"
+  }
+]);
